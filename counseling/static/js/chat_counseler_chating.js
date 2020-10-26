c02125fb304187d33dbe641d4f962241988babd1
fjdksl546 <50150719+fjdksl546@users.noreply.github.com>
2020-10-26 09:27:37 +0900

    Update chat_counseler_chating.js

--- counseling/static/js/chat_counseler_chating.js
+++ counseling/static/js/chat_counseler_chating.js
@@ -137,7 +137,7 @@ function chatWS() { //연결
 }
 
 function chatMessage(para) { //수신 데이터 분별 및 함수 호출
-    jsonParsing = json.parse(para.data);
+    jsonParsing = JSON.parse(para.data);
 	console.log(jsonParsing)
     switch(jsonParsing.type) {
         case 'apply':
