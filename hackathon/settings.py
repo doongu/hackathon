f1dc89ce8e1f4dadde8eef6101d80d8ac0544bf1
fjdksl546 <fjdksl5464@gmail.com>
2020-10-30 13:58:11 +0900

    z

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
        os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT=os.path.join(BASE_DIR, 'counseling/static')
 
 ALGORITHM = 'HS256'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
